e25ee9bcf642e2db2e979acef38288f8041e2f4b
Bicon <minhazu.alam142@northsouth.edu>
2020-06-24 12:32:27 +0600

    Bug fixed and new feature update

--- KidsCounting/app/src/main/java/com/example/kidscounting/AppleCount_activity.java
+++ KidsCounting/app/src/main/java/com/example/kidscounting/AppleCount_activity.java
@@ -3,6 +3,7 @@ package com.example.kidscounting;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
@@ -10,16 +11,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.io.IOException;
 
 public class AppleCount_activity extends AppCompatActivity implements View.OnClickListener {
-    private MainActivity mainActivity;
     private ImageButton imageButton1, imageButton2, imageButton3, imageButton4, imageButton5, imageButton6,
-            imageButton7, imageButton8, imageButton9, imageButton10, repeat, cross_btn, mute;
+                        imageButton7, imageButton8, imageButton9, imageButton10, repeat, cross_btn, mute;
     private ImageView AppleShow;
     private MediaPlayer sound1, sound2, sound3, sound4, sound5, sound6, sound7, sound8, sound9,sound10;
     private int state=1;
@@ -35,8 +38,6 @@ public class AppleCount_activity extends AppCompatActivity implements View.OnCli
         // always open in Landscape mode
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        // object of main activity
-        mainActivity = new MainActivity();
         // find Image view
         AppleShow = findViewById(R.id.Apple_ImageView);
         //find all button
@@ -68,46 +69,20 @@ public class AppleCount_activity extends AppCompatActivity implements View.OnCli
         mute.setOnClickListener(this);
         repeat.setOnClickListener(this);
         cross_btn.setOnClickListener(this);
-
-        //create mediaPlayer for all sounds
-        sound1 = MediaPlayer.create(this, R.raw.one);
-        sound2 = MediaPlayer.create(this, R.raw.two);
-        sound3 = MediaPlayer.create(this, R.raw.three);
-        sound4 = MediaPlayer.create(this, R.raw.four);
-        sound5 = MediaPlayer.create(this, R.raw.five);
-        sound6 = MediaPlayer.create(this, R.raw.six);
-        sound7 = MediaPlayer.create(this, R.raw.seven);
-        sound8 = MediaPlayer.create(this, R.raw.eight);
-        sound9 = MediaPlayer.create(this, R.raw.nine);
-        sound10 = MediaPlayer.create(this, R.raw.ten);
-
-
     }
 
     @Override
     public void onClick(View view) {
-        if (view == mute) {
-            if(state==0){
-                mute.setImageResource(R.drawable.btn_mute);
-                sound1.reset();
-                sound2.reset();
-                sound3.reset();
-                sound4.reset();
-                sound5.reset();
-                sound6.reset();
-                sound7.reset();
-                sound8.reset();
-                sound9.reset();
-                sound10.reset();
-                state=state+1;
-            }
-            else {
-                mute.setImageResource(R.drawable.btn_unmute);
-                state=state-1;
-        }
+        if (view == cross_btn){
+            Fade_Animation();
+            Intent intent = new Intent(AppleCount_activity.this,Home_Activities.class);
+            startActivity(intent);
+            finish();
         }
 
         if (view == imageButton1) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton1.startAnimation(animation);
             AppleShow.setImageResource(R.drawable.object_1);
             if (sound1 != null) {
                 sound1.start();
@@ -115,60 +90,133 @@ public class AppleCount_activity extends AppCompatActivity implements View.OnCli
         }
 
         if (view == imageButton2) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton2.startAnimation(animation);
             AppleShow.setImageResource(R.drawable.object_2);
             if (sound2 != null) {
                 sound2.start();
             }
         }
+
         if (view == imageButton3) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton3.startAnimation(animation);
             AppleShow.setImageResource(R.drawable.object_3);
             if (sound3 != null) {
                 sound3.start();
             }
         }
+
         if (view == imageButton4) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton4.startAnimation(animation);
             AppleShow.setImageResource(R.drawable.object_4);
             if (sound4 != null) {
                 sound4.start();
             }
         }
+
         if (view == imageButton5) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton5.startAnimation(animation);
             AppleShow.setImageResource(R.drawable.object_5);
-            if (sound5 != null) {
-                sound5.start();}
+            if (sound5 !=null) {
+                sound5.start();
             }
-            if (view == imageButton6) {
-                AppleShow.setImageResource(R.drawable.object_6);
+            }
+
+        if (view == imageButton6) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton6.startAnimation(animation);
+            AppleShow.setImageResource(R.drawable.object_6);
                 if (sound6 != null) {
                     sound6.start();
                 }
             }
-            if (view == imageButton7) {
-                AppleShow.setImageResource(R.drawable.object_7);
+
+        if (view == imageButton7) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton7.startAnimation(animation);
+            AppleShow.setImageResource(R.drawable.object_7);
                 if (sound7 != null) {
                     sound7.start();
                 }
             }
-            if (view == imageButton8) {
-                AppleShow.setImageResource(R.drawable.object_8);
+
+        if (view == imageButton8) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton8.startAnimation(animation);
+            AppleShow.setImageResource(R.drawable.object_8);
                 if (sound8 != null) {
                     sound8.start();
                 }
             }
-            if (view == imageButton9) {
-                AppleShow.setImageResource(R.drawable.object_9);
+
+        if (view == imageButton9) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton9.startAnimation(animation);
+            AppleShow.setImageResource(R.drawable.object_9);
                 if (sound9 != null) {
                     sound9.start();
                 }
             }
-            if (view == imageButton10) {
-                AppleShow.setImageResource(R.drawable.object_10);
+
+        if (view == imageButton10) {
+            Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+            imageButton10.startAnimation(animation);
+            AppleShow.setImageResource(R.drawable.object_10);
                 if (sound10 != null) {
                     sound10.start();
                 }
             }
 
-
+        if (view == mute) {
+            if(state==0){
+                Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+                mute.startAnimation(animation);
+                mute.setImageResource(R.drawable.btn_mute);
+              if(sound1==null||sound2==null||sound3==null||sound4==null||sound5==null||sound6==null||sound7==null||sound8==null
+                      ||sound9==null||sound10==null){
+                  Toast.makeText(getApplicationContext(),"Sound Muted",Toast.LENGTH_SHORT).show();
+                  state=state+1;
+              } else {
+                  Toast.makeText(getApplicationContext(),"Sound Muted",Toast.LENGTH_SHORT).show();
+                  sound1.stop();
+                  sound2.stop();
+                  sound3.stop();
+                  sound4.stop();
+                  sound5.stop();
+                  sound6.stop();
+                  sound7.stop();
+                  sound8.stop();
+                  sound9.stop();
+                  sound10.stop();
+                  state=state+1;}
+            }
+            else {
+                Animation animation = AnimationUtils.loadAnimation(this,R.anim.slide);
+                mute.startAnimation(animation);
+                mute.setImageResource(R.drawable.btn_unmute);
+                //create mediaPlayer for all sounds
+                sound1 = MediaPlayer.create(this, R.raw.one);
+                sound2 = MediaPlayer.create(this, R.raw.two);
+                sound3 = MediaPlayer.create(this, R.raw.three);
+                sound4 = MediaPlayer.create(this, R.raw.four);
+                sound5 = MediaPlayer.create(this, R.raw.five);
+                sound6 = MediaPlayer.create(this, R.raw.six);
+                sound7 = MediaPlayer.create(this, R.raw.seven);
+                sound8 = MediaPlayer.create(this, R.raw.eight);
+                sound9 = MediaPlayer.create(this, R.raw.nine);
+                sound10 = MediaPlayer.create(this, R.raw.ten);
+                state=state-1;
             }
         }
+    }
+
+    public void Fade_Animation(){
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade);
+        cross_btn.startAnimation(animation);
+    }
+
+}
 
